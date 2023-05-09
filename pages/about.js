d0411d8feeff738830626aed20862a4c1169e138
mochamadyudi <myudisobari12@gmail.com>
2023-05-09 07:21:21 +0700

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+export default function About(){
+    return(
+        <>
+            <main>
+                page about
+
+            </main>
+            <footer>
+                ini footer
+            </footer>
+        </>
+    )
+}
